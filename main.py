865cb867214ba8ee6afe4373b8f910e01f1b3032
tinali0 <xzl5423@psu.edu>
2020-09-03 04:48:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+gradepoint1 = input("Enter your course 1 letter grade: ")
+credit1 = input("Enter your course 1 credit: ")
+if gradepoint1 == "A" or gradepoint1 == "a":
+  print("Grade point for course 1 is: 4.0")
+elif gradepoint1 == "A-" or gradepoint1 == "a-":
+  print("Grade point for course 1 is: 3.67")
+elif gradepoint1 == "B+" or gradepoint1 == "b+":
+  print("Grade point for course 1 is: 3.33")
+elif gradepoint1 == "B" or gradepoint1 == "b":
+  print("Grade point for course 1 is: 3.0")
+elif gradepoint1 == "B-" or gradepoint1 == "b-":
+  print("Grade point for course 1 is: 2.67")
+elif gradepoint1 == "C+" or gradepoint1 == "c+":
+  print("Grade point for course 1 is: 2.33")
+elif gradepoint1 == "C" or gradepoint1 == "c":
+  print("Grade point for course 1 is: 2.0")
+elif gradepoint1 == "D" or gradepoint1 == "d":
+  print("Grade point for course 1 is: 1.0")
+elif gradepoint1 == "F" or gradepoint1 == "f":
+  print("Grade point for course 1 is: 0.00")
+else:
+  print("Grade point for course 1 is: 0.00")
+
+gradepoint2 = input("Enter your course 2 letter grade: ")
+credit1 = input("Enter your course 2 credit: ")
